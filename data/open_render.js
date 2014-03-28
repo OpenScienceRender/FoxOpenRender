3b4da545b2eeba1cf9714aa336ec1939a2089a4e
James Hetherington <j.hetherington@ucl.ac.uk>
2014-03-28 14:40:59 +0000

    Nonfunctional fix attempt

--- data/open_render.js
+++ data/open_render.js
@@ -1,5 +1,6 @@
+window.alert("Payload inserted")
 self.port.on("replaceContent", function(url) {
-
+window.alert("Replacing with"+url)
 var content=[
   '<div class="blob-wrapper">',
     '<iframe width="100%" height="1000px" src="'+url+'" sandbox="allow-scripts allow-same-origin">Viewer requires iframe.</iframe>',
